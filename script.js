45bf5ab900dae54a0e39d0041233fa40a9edb280
Jimno <menaoir.jimmy@gmail.com>
2022-06-22 15:09:42 +0100

    few effect changes

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
-
-function createGrid() {
+function changeColor() {
+   let color = document.getElementById('col').value
+const col = document.getElementById('col')
+col.addEventListener('mouseout', e => {
+    col.style.color = e.target.value
+      color = e.target.value
+  let root = document.documentElement;
+root.style.setProperty('--clr-change', color)    
+      }); 
+} 
+function createGrid () {
     const gridy = document.getElementById('container')
-
-    for (let i = 1; i < 64; i++) {
+      
+ const gridsSize = document.getElementById('gridsSize').addEventListener('change', e => {
+        let m = e.target.value
+        document.getElementById('gridsSize').disabled = true
+    for (let i = 1; i <= m; i++) {
+          const colorBtn = document.getElementById('colorBtn')
+             colorBtn.addEventListener('click', e => {
+                location.reload();
+                return false;
+             });
         let myRow = document.createElement('tr')
         myRow.id = 'row' + i ;
             gridy.appendChild(myRow)
             let rowW = document.getElementById('row' + i);
-                myRow.addEventListener('mouseover', event => {
-                event.target.style= "background-color : blue ;"
-            })
-            
-        
-        for (let j = 1; j < 64; j++) {
+            let color = document.getElementById('col').value
+            const col = document.getElementById('col')
+            gridy.addEventListener('mouseover', e => {
+               e.target.style = `background-color : ${color};`
+            });   
+            col.addEventListener('mouseout', e => {
+              col.style.color = e.target.value
+                color = e.target.value
+                });  
+        for (let j = 1; j <= m ; j++) {
         let myColumn = document.createElement('td')
         myColumn.id = 'column' + j
             rowW.appendChild(myColumn)
 
-            const buttons = document.querySelectorAll('button')
-            buttons.forEach((button) => {
-            button.addEventListener('click', e => {
-            myColumn.style = "background-color: aliceblue;"
-            })
-            })
-        }
-        
+            const clearBtn = document.querySelector('.clearBtn')
+            clearBtn.addEventListener('click', e => {
+            myColumn.style = "background-color: white;"
+            });
+            const clearInp = document.querySelector('.clearInp')
+            clearInp.addEventListener('click', e => {
+            myColumn.style = "background-color: white;"
+            });   
+        };
+    };    
+    function randomizeColor() {
+        const randomR = Math.floor(Math.random() * 256)
+        const randomG = Math.floor(Math.random() * 256)
+        const randomB = Math.floor(Math.random() * 256)   
+        return `rgb(${randomR}, ${randomG}, ${randomB})`
     };
-       
+        const rainbowBtn = document.getElementById('rainbowBtn')
+        rainbowBtn.addEventListener('click', () => {
+                gridy.addEventListener('mouseover', e => {
+                e.target.style = `background-color : ${randomizeColor()};`
+                });
+        })
+  })    
 };
+changeColor()
 createGrid()
