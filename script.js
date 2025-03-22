15c1494687d6994ae18336ba97aa7b8883ed84ab
jimno <98581961+DerJimno@users.noreply.github.com>
2025-03-22 12:49:30 +0100

    remake of everything

--- script.js
+++ script.js
@@ -1,66 +1,94 @@
-function changeColor() {
-   let color = document.getElementById('col').value
-const col = document.getElementById('col')
-col.addEventListener('mouseout', e => {
-    col.style.color = e.target.value
-      color = e.target.value
-  let root = document.documentElement;
-root.style.setProperty('--clr-change', color)    
-      }); 
-} 
-function createGrid () {
-    const gridy = document.getElementById('container')
-      
- const gridsSize = document.getElementById('gridsSize').addEventListener('change', e => {
-        let m = e.target.value
-        document.getElementById('gridsSize').disabled = true
-    for (let i = 1; i <= m; i++) {
-          const colorBtn = document.getElementById('colorBtn')
-             colorBtn.addEventListener('click', e => {
-                location.reload();
-                return false;
-             });
-        let myRow = document.createElement('tr')
-        myRow.id = 'row' + i ;
-            gridy.appendChild(myRow)
-            let rowW = document.getElementById('row' + i);
-            let color = document.getElementById('col').value
-            const col = document.getElementById('col')
-            gridy.addEventListener('mouseover', e => {
-               e.target.style = `background-color : ${color};`
-            });   
-            col.addEventListener('mouseout', e => {
-              col.style.color = e.target.value
-                color = e.target.value
-                });  
-        for (let j = 1; j <= m ; j++) {
-        let myColumn = document.createElement('td')
-        myColumn.id = 'column' + j
-            rowW.appendChild(myColumn)
+const container = document.querySelector(".container");
+const baseColor = "#ff8040" 
 
-            const clearBtn = document.querySelector('.clearBtn')
-            clearBtn.addEventListener('click', e => {
-            myColumn.style = "background-color: white;"
-            });
-            const clearInp = document.querySelector('.clearInp')
-            clearInp.addEventListener('click', e => {
-            myColumn.style = "background-color: white;"
-            });   
-        };
-    };    
-    function randomizeColor() {
-        const randomR = Math.floor(Math.random() * 256)
-        const randomG = Math.floor(Math.random() * 256)
-        const randomB = Math.floor(Math.random() * 256)   
-        return `rgb(${randomR}, ${randomG}, ${randomB})`
-    };
-        const rainbowBtn = document.getElementById('rainbowBtn')
-        rainbowBtn.addEventListener('click', () => {
-                gridy.addEventListener('mouseover', e => {
-                e.target.style = `background-color : ${randomizeColor()};`
-                });
-        })
-  })    
+function createGrid(size = 16) {
+  for (i = 0; i < (size * size); i++){
+    const square = document.createElement("div");
+    square.className = "square"
+    container.appendChild(square);
+  };
+  colorSquare();
+  changeColor();
+  randomizeSquare();
+  clearGrid();
 };
-changeColor()
-createGrid()
+
+function changeGrid() {
+  const range = document.querySelector(".range")
+  range.addEventListener('change', e => {
+    container.innerHTML = '';
+    const value = e.target.value
+    for (i = 0; i < ( value * value); i++){
+      const square = document.createElement("div");
+      square.className = "square"
+      container.appendChild(square);
+    };
+    const squares = document.querySelectorAll(".square")
+    squares.forEach((square) => {
+      square.style.flexBasis = `calc(100% / ${value})`;
+    });
+    colorSquare();
+    changeColor();
+    randomizeSquare();
+    setTheme();
+    clearGrid();
+  });
+};
+
+function colorSquare(color = baseColor) {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => { 
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = `${color}`;
+    });
+  });
+};
+
+function changeColor(){
+  const color = document.querySelector(".color");
+  color.addEventListener('click',  () => {
+    color.addEventListener('mouseout', e => {
+      colorSquare(e.target.value)
+      setTheme(e.target.value)    
+    });
+  });
+}; 
+
+function getRandomColor() {
+  const randomR = Math.floor(Math.random() * 256)
+  const randomG = Math.floor(Math.random() * 256)
+  const randomB = Math.floor(Math.random() * 256)   
+  return `rgb(${randomR}, ${randomG}, ${randomB})`
+};
+
+function randomizeSquare() {
+  const rainbowBtn = document.querySelector("#rainbowBtn")
+  rainbowBtn.addEventListener("click", () => {
+  const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => { 
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = `${getRandomColor()}`;
+      });
+    });
+  });
+};
+
+function setTheme (color = baseColor){
+  const root = document.documentElement
+  root.style.setProperty('--clr-change', color)
+  const inputColor = document.querySelector(".color")
+  inputColor.value = color
+};
+
+function clearGrid() {
+  const clearBtn = document.querySelector(".clearBtn")
+  const squares = document.querySelectorAll(".square");
+  clearBtn.addEventListener("click", () => {
+    squares.forEach((square) => { 
+      square.style.backgroundColor = "white";
+    });
+  });
+};
+
+createGrid();
+changeGrid();
